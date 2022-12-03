01580866ab4f2558ef83173e8a7979e6322cef7e
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2022-12-03 10:20:37 -0800

    Added components, worked on navbar, set up modal state

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
+// Components
+import Head from 'next/head';
+
 // Styles
 import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta charSet='utf-8' />
+        <meta name="description" content="Cafe website" />
+        <meta name="keywords" content="Boris Shvidchenko, Portfolio, Cafe, Website" />
+        <meta name="author" content="Boris Shvidchenko" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Cafe</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,29 @@
 // Components
-import Head from 'next/head'
+import Navbar from '../components/Navbar';
+import Header from '../components/Header';
+import About from '../components/About';
+import Menu from '../components/Menu';
+import Contact from '../components/Contact';
+import Footer from '../components/Footer';
+import Modal from '../components/Modal';
+
+// Hooks
+import { useState } from 'react';
 
 export default function Home() {
+
+  // State
+  const [modal, setModal] = useState(false);
+
   return (
-    <div>
-      <Head>
-        <title>Cafe</title>
-      </Head>     
-    </div>
+    <main className='flex flex-col'>
+      {/* <Modal /> */}
+      <Navbar />
+      {/* <Header /> */}
+      {/* <About /> */}
+      {/* <Menu /> */}
+      {/* <Contact /> */}
+      {/* <Footer /> */}
+    </main>
   )
 }
