e0b369dcafed181f5b3321092a7549f2d124914e
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2022-12-04 12:19:01 -0800

    Added a hardcoded version of a drink menu to a file

--- components/Menu.js
+++ components/Menu.js
@@ -2,8 +2,8 @@ export default function Menu() {
     return (
         <div className='page-sizing mt-5 md:mt-10'>
             <div id='menu' className='nav-placeholder-div'></div>
-            <h1 className='body-headers'>Our Products</h1>
-            <p className='lg:hidden mx-11 md:mx-1 mb-7 md:mb-10'></p>
+            <h1 className='body-headers lg:mb-6'>Our Products</h1>
+            <p className='mx-11 md:mx-1'>We serve traditional coffee drinks, hot and cold, and have a selection of daily prepped foods.</p>
         </div>
     )
 }
---
+++ drinksMenuData.js
@@ -0,0 +1,164 @@
+// Note: The data was obtained from https://sampleapis.com/api-list/coffee. Some of the images and descriptions have been modified.
+
+export const drinks = [
+    {
+      "title": "Latte",
+      "description": "The latte is comprised of a shot of espresso and steamed milk with just a touch of foam.",
+      "ingredients": [
+        "Espresso",
+        "Steamed Milk"
+      ],
+      "image": "https://images.pexels.com/photos/312418/pexels-photo-312418.jpeg",
+      "id": 2
+    },
+    {
+      "title": "Cappuccino",
+      "description": "Cappuccino is a latte made with more foam than steamed milk, often with a sprinkle of cocoa powder or cinnamon on top. Sometimes you can find variations that use cream instead of milk or ones that throw in flavor shot, as well.",
+      "ingredients": [
+        "Espresso",
+        "Steamed Milk",
+        "Foam"
+      ],
+      "image": "https://images.pexels.com/photos/6166478/pexels-photo-6166478.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      "id": 3
+    },
+    {
+      "title": "Americano",
+      "description": "With a similar flavor to black coffee, the americano consists of an espresso shot diluted in hot water.",
+      "ingredients": [
+        "Espresso",
+        "Hot Water"
+      ],
+      "image": "https://images.pexels.com/photos/4195599/pexels-photo-4195599.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      "id": 4
+    },
+    {
+      "title": "Espresso",
+      "description": "An espresso shot can be served solo or used as the foundation of most coffee drinks, like lattes and macchiatos.",
+      "ingredients": [
+        "1oz Espresso"
+      ],
+      "image": "https://images.pexels.com/photos/685527/pexels-photo-685527.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      "id": 5
+    },
+    {
+      "title": "Doppio",
+      "description": "A double shot of espresso, the doppio is perfect for putting extra pep in your step.",
+      "ingredients": [
+        "2oz Espresso"
+      ],
+      "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Doppio.jpg/2560px-Doppio.jpg",
+      "id": 6
+    },
+    {
+      "title": "Cortado",
+      "description": "A cortado has the perfect balance of espresso and warm steamed milk. The milk is used to cut back on the espresso’s acidity.",
+      "ingredients": [
+        "1oz Espresso",
+        "1oz Steamed Milk"
+      ],
+      "image": "https://upload.wikimedia.org/wikipedia/commons/1/16/Caf%C3%A9Cortado%28Tallat%29.jpg",
+      "id": 7
+    },
+    {
+      "title": "Red Eye",
+      "description": "A full cup of hot coffee with an espresso shot mixed in.",
+      "ingredients": [
+        "Coffee",
+        "Espresso"
+      ],
+      "image": "https://upload.wikimedia.org/wikipedia/commons/f/f7/Linea_doubleespresso.jpg",
+      "id": 8
+    },
+    {
+      "title": "Galão",
+      "description": "Originating in Portugal, this hot coffee drink is closely related to the latte and cappuccino.",
+      "ingredients": [
+        "Espresso",
+        "Foamed milk"
+      ],
+      "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Gal%C3%A3o.jpg/1280px-Gal%C3%A3o.jpg",
+      "id": 9
+    },
+    {
+      "title": "Lungo",
+      "description": "A lungo is a long-pull espresso. The longer the pull, the more caffeine there is and the more ounces you can enjoy.",
+      "ingredients": [
+        "Long pulled espresso"
+      ],
+      "image": "https://images.pexels.com/photos/1252195/pexels-photo-1252195.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      "id": 10
+    },
+    {
+      "title": "Macchiato",
+      "description": "The macchiato is an espresso-based drink that has a small amount of foam on top.",
+      "ingredients": [
+        "Espresso",
+        "Foam"
+      ],
+      "image": "https://images.pexels.com/photos/62428/pexels-photo-62428.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      "id": 11
+    },
+    {
+      "title": "Mocha",
+      "description": "The mocha is a chocolate espresso drink with steamed milk and foam.",
+      "ingredients": [
+        "Espresso",
+        "Steamed Milk",
+        "Chocolate"
+      ],
+      "image": "https://upload.wikimedia.org/wikipedia/commons/f/f6/Mocaccino-Coffee.jpg",
+      "id": 12
+    },
+    {
+      "title": "Ristretto",
+      "description": "Ristretto is an espresso shot. It uses less hot water which creates a sweeter flavor compared to the bitter taste of a traditional shot of espresso or a doppio.",
+      "ingredients": [
+        "Short pulled espresso"
+      ],
+      "image": "https://upload.wikimedia.org/wikipedia/commons/1/12/Doppio_ristretto_Chiang_Mai.jpg",
+      "id": 13
+    },
+    {
+      "title": "Flat White",
+      "description": "This Aussie-born drink is basically a cappuccino without the foam or chocolate sprinkle. It’s an espresso drink with steamed milk.",
+      "ingredients": [
+        "Espresso",
+        "Steamed Milk"
+      ],
+      "image": "https://upload.wikimedia.org/wikipedia/commons/6/6b/Flat_white_coffee_with_pretty_feather_pattern.jpg",
+      "id": 14
+    },
+    {
+      "title": "Affogato",
+      "description": "The affogato is served with a scoop of ice cream and a shot of espresso, or two.",
+      "ingredients": [
+        "Espresso",
+        "Ice cream"
+      ],
+      "image": "https://upload.wikimedia.org/wikipedia/commons/1/17/Vinoteca%2C_Smithfield%2C_London_%284485849609%29.jpg",
+      "id": 15
+    },
+    {
+      "title": "Café au Lait",
+      "description": "Café au lait is perfect for the coffee minimalist who wants a bit more flavor.",
+      "ingredients": [
+        "Coffee",
+        "Steamed Milk"
+      ],
+      "image": "https://upload.wikimedia.org/wikipedia/commons/0/06/Latte_art.jpg",
+      "id": 16
+    },
+    {
+      "title": "Irish",
+      "description": "Irish coffee consists of black coffee, whiskey and sugar, topped with whipped cream.",
+      "ingredients": [
+        "Coffee",
+        "Whiskey",
+        "Sugar",
+        "Cream"
+      ],
+      "image": "https://upload.wikimedia.org/wikipedia/commons/6/61/Irish_coffee_glass.jpg",
+      "id": 17
+    }
+  ]
