6b73310e9656aec4dcb8fab78c457dff5280ffd8
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2022-12-07 08:43:04 -0800

    Worked on adding content/style to jobs page

--- components/JobListings.js
+++ components/JobListings.js
@@ -5,8 +5,15 @@ export default function JobListings() {
     return (
         <div className='page-sizing pt-10'>
             <h1 className='body-headers'>Join our Team!</h1>
-            <p className='mx-10 md:ml-0'>We currently dont have any openings but will post new positions here when they are  available, check back soon! Or, send us a resume ahead of time, and we'll get back to you when an opportunity comes up.</p>
-            <p className='ml-10 md:ml-0 mt-5 mb-9 text-lg italic'>contact@westoceansidecoffee.com</p>
+            <p className='mx-10 md:ml-0'>We currently have 2 openings for a Manager and a part time Barista position, we will post new positions here when they are  available as well so make sure to check back soon! Dont see a posting you like? Send us a resume anyway, and we'll get back to you when another opportunity comes up.</p>
+            <p className='ml-10 md:ml-0 mt-5 text-lg italic'>contact@westoceansidecoffee.com</p>
+            <h3 className='text-xl md:text-2xl ml-10 md:ml-0 mb-4 mt-5'>Open Positions</h3>
+
+            <table className='mb-10 ml-10 md:ml-0 border-separate border-spacing-x-6 border-spacing-y-1'>
+                <tr><td className='jobs-titles'>Manager</td><td className='jobs-link'>{`Apply Now >`}</td></tr>
+                <tr><td className='jobs-titles'>{`Barista (Part-Time)`}</td><td className='jobs-link'>{`Apply Now >`}</td></tr>
+             </table>
+
             <Image 
                 src='https://images.pexels.com/photos/2788792/pexels-photo-2788792.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
                 alt='A group of people in a coffee shop.'
