3b095f115a89eca48a67aa5309efaf8b0105907e
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2022-12-08 09:40:57 -0800

    Update

--- components/Header.js
+++ components/Header.js
@@ -25,7 +25,7 @@ export default function Header() {
             <section className='absolute top-4 sm:top-10 left-4 sm:left-10 p-10'>
                 <h1 className={`header-h1 mb-3 ${mobileView?.width < '485' ? 'text-3xl' : ''}`}>High quality coffee</h1>
                 <h1 className={`header-h1 mb-8 ${mobileView?.width < '485' ? 'text-3xl mb-6' : ''}`}>since 2018</h1>
-                <hr className={`mb-8 border w-52 lg:w-64 xl:w-80 2xl:w-96 ${mobileView?.width < '485' ? 'mb-5' : ''}`}/>
+                <hr className={`mb-8 border w-52 lg:w-64 xl:w-80 2xl:w-96 ${mobileView?.width < '485' ? 'hidden' : ''}`}/>
                 <section className='flex items-center space-x-3'>
                     <img className='cursor-pointer w-10 h-10' src='/icons/instagram.png' alt='Instagram logo' />
                     <img className='cursor-pointer w-9 h-9' src='/icons/facebook.png' alt='Facebook logo' />
